ba4abf1920a797dfd72c5747cf5afa0bea92e958
denshi25 <23cm0125@jec.ac.jp>
2024-05-24 15:18:56 +0900

    Initial Commit

---
+++ RestaurantSearcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestaurantSearcher
+//
+//  Created by cmStudent on 2024/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
